a3d38dbb46342d5f04488cd1d6d458409ff30404
Andrey Nightheaven <sos1995@list.ru>
2017-02-18 16:11:15 +0300

    ALL FILE ADD

---
+++ add_post.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 18.02.2017
+ * Time: 15:34
+ */
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 18.02.2017
+ * Time: 15:35
+ */
+
