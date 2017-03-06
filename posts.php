e071884ce3e1f7e9884f87f351139998719c975d
Andrey Nightheaven <sos1995@list.ru>
2017-03-06 19:30:50 +0300

    ЧТО-ТО ЗАКОМИТИЛ

--- posts.php
+++ posts.php
@@ -1,3 +1,7 @@
+
+<link rel="stylesheet" href="css/bootstrap.css">
+
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +9,11 @@
  * Date: 18.02.2017
  * Time: 15:34
  */
+
+
+?>
+<p>Имя поста <input name="title" type="text"></p>
+<p>Текст поста <textarea name="text" rows="N" cols="N" value=""></textarea></p>
+
+<button type="button" class="btn btn-success" onclick="location.href='add_post.php' ;">Создать новый пост</button>
+<button type="button" class="btn btn-danger" onclick="location.href='index.php';">назад</button>
